b85d02771a85023151c1f5a7a95647551ce0d408
lenovo <lenovo@LAPTOP-8NCIJN2T>
2021-12-15 12:24:25 +0530

    commit4

---
+++ crm.practice4/src/test/java/crm/practice4/craeteProject.java
@@ -0,0 +1,13 @@
+package crm.practice4;
+
+import org.testng.annotations.Test;
+
+public class craeteProject {
+	
+	@Test
+	public void createProject() {
+		
+		System.out.println("Hello");
+	}
+
+}
